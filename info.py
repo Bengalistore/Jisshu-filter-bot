efc761259807500ed5e6e037320056b1548d0385
Bengalistore <krjnstore@gmail.com>
2025-08-09 23:25:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,8 +38,8 @@ CHANNELS = [
 ]
 
 # ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", ""))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002534798811"))
+AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", "-1002698222521"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002591125289"))
 LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "-1002591125289"))
 LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "-1002591125289"))
